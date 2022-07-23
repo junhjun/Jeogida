e1b4fc1bfda38d64dfa978bfb2b5830244b1fcae
Minjune <minjune8506@gmail.com>
2022-07-23 14:57:54 +0900

    feat : FindCar 페이지에 menu, appBar 추가, navigator 기능 추가

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,20 @@ import 'package:parking_spot_frontend/change_password_view.dart';
 import 'package:parking_spot_frontend/home_view.dart';
 import 'package:parking_spot_frontend/login_view.dart';
 
+import 'FindCar/find_car.dart';
+import 'FindCar/register_web_webview_stub.dart'
+    if (dart.library.html) 'register_web_webview.dart';
+
 void main() {
+  registerWebViewWebImplementation();
   runApp(
     MaterialApp(
       title: "Parking Spot",
-      initialRoute: HomeView.routeName,
+      initialRoute: FindCar.routeName,
       onGenerateRoute: (RouteSettings settings) {
         switch (settings.name) {
-          case HomeView.routeName:
-            return MaterialPageRoute(builder: (context) => HomeView());
+          case FindCar.routeName:
+            return MaterialPageRoute(builder: (context) => FindCar());
           case LoginView.routeName:
             return MaterialPageRoute(builder: (context) => LoginView());
           case MyPageView.routeName:
--- lib/menu.dart
+++ lib/menu.dart
@@ -16,6 +16,9 @@ class _MyMenuState extends State<MyMenu> {
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
+      if (_selectedIndex == 0) {
+        Navigator.pushReplacementNamed(context, "/FindCar");
+      }
       if (_selectedIndex == 2) {
         Navigator.pushReplacementNamed(context, "/BookMarkView");
       }

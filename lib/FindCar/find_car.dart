676f8d47f23f3e1d0ff055c63de125a7b986beb8
Minjune <minjune8506@gmail.com>
2022-08-01 22:01:44 +0900

    feat : find_car UI 수정

--- lib/FindCar/find_car.dart
+++ lib/FindCar/find_car.dart
@@ -14,6 +14,16 @@ class _MyAppState extends State<FindCar> {
 
   final _valueList = ['20누 0856', '11가 1234', '22나 2345', '33다 3456'];
   var _selectedValue = '20누 0856';
+  final _dropdownTextStyle = TextStyle(fontSize: 13, color: Colors.black);
+  final _dropdownIcon = Icon(Icons.arrow_drop_down, size: 30);
+  var _currentArea = "B1층 A구역 30번";
+  var _currentSpot = "개봉 현대아파트";
+  var _parkingDuration = "3시간 30분";
+  final _currentAreaTextStyle = const TextStyle(
+      fontSize: 20, color: Colors.cyan, fontWeight: FontWeight.bold);
+  final _dropDownItemTextStyle =
+      const TextStyle(fontSize: 13, color: Colors.black);
+  final _infoTextStyle = const TextStyle(fontSize: 15, color: Colors.grey);
 
   @override
   Widget build(BuildContext context) {
@@ -22,132 +32,106 @@ class _MyAppState extends State<FindCar> {
       appBar: CustomAppBar(appBar: AppBar(), title: "내 차 찾기"),
       bottomNavigationBar: MyMenu(selectedIndex: 0),
       body: Container(
-        alignment: Alignment.center,
-        margin: EdgeInsets.fromLTRB(20, 30, 20, 70),
-        padding: EdgeInsets.fromLTRB(20, 0, 20, 10),
+        margin: EdgeInsets.all(30),
         decoration: BoxDecoration(
-            border: Border.all(color: Color(0xffe3e2e2)),
+            border: Border.all(color: Colors.grey),
             borderRadius: BorderRadius.all(Radius.circular(8.0))),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // 차량번호 드롭다운 버튼
-            Container(
-                width: 160,
-                height: 25,
-                alignment: Alignment.center,
-                padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                decoration: BoxDecoration(
-                    border: Border.all(color: Color(0xffd2d0d0)),
-                    borderRadius: BorderRadius.all(Radius.circular(8.0))),
-                child: DropdownButton(
-                    value: _selectedValue,
-                    items: _valueList.map((value) {
-                      return DropdownMenuItem(
-                        value: value,
-                        child: Text('차량번호 | ' + value),
-                      );
-                    }).toList(),
-                    onChanged: (String? value) {
-                      setState(() {
-                        _selectedValue = value!;
-                      });
-                    },
-                    underline: Container(), // 밑 줄 제거
-                    style: TextStyle(fontSize: 13, color: Colors.black),
-                    icon: Padding(
-                      padding: EdgeInsets.all(0),
-                      child: Icon(Icons.arrow_drop_down, size: 23),
-                    ),
-                    iconEnabledColor: Colors.grey)),
-
-            // 주차 정보
-            Container(
-              // margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-              height: 90,
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('B1층 A구역 30번',
-                        style: TextStyle(
-                            color: Colors.cyan,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w700)),
-                    Column(
-                      children: [
-                        Text('위치 : 개봉 현대아파트',
-                            style: TextStyle(color: Colors.grey, fontSize: 13)),
-                        Text('주차시간 : 3시간 30분',
-                            style: TextStyle(color: Colors.grey, fontSize: 13))
-                      ],
-                    )
-                  ]),
-            ),
-
-            // 아이콘 정보
-            Container(
-                height: 60,
-                padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                decoration: BoxDecoration(
-                    border: Border(top: BorderSide(color: Color(0xffe3e2e2)))),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Container(
-                      padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
-                      child: Row(
-                        children: [
-                          Icon(Icons.circle,
-                              size: 7, color: Colors.deepOrangeAccent),
-                          Text('현재 위치',
-                              style:
-                                  TextStyle(color: Colors.grey, fontSize: 12))
-                        ],
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
-                      child: Row(
-                        children: [
-                          Icon(Icons.rectangle,
-                              size: 12, color: Colors.deepOrangeAccent),
-                          Text('주차 위치',
-                              style:
-                                  TextStyle(color: Colors.grey, fontSize: 12))
-                        ],
-                      ),
-                    )
-                  ],
-                )),
-
-            // 웹뷰
-            Container(
-                alignment: Alignment.center,
-                child: Column(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.fromLTRB(0, 0, 0, 20),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Color(0xffe3e2e2))),
-                      width: 350,
-                      height: 250,
-                      child: WebView(
-                        initialUrl:
-                            "http://3.37.217.255:8080/swagger-ui/index.html#/",
-                        javascriptMode: JavascriptMode.unrestricted,
-                        onWebViewCreated:
-                            (WebViewController webViewController) {
-                          _controller = webViewController;
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.grey),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey, blurRadius: 5, offset: Offset(4, 8))
+              ],
+              borderRadius: BorderRadius.all(Radius.circular(8.0))),
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.only(bottom: 20),
+                child: Container(
+                    child: DropdownButton(
+                        value: _selectedValue,
+                        items: _valueList.map((value) {
+                          return DropdownMenuItem(
+                            value: value,
+                            child: Text('차량번호 | ' + value,
+                                style: _dropDownItemTextStyle),
+                          );
+                        }).toList(),
+                        onChanged: (String? value) {
+                          setState(() {
+                            _selectedValue = value!;
+                          });
                         },
+                        underline: null,
+                        style: _dropdownTextStyle,
+                        icon: _dropdownIcon,
+                        iconEnabledColor: Colors.grey)),
+              ),
+              Container(
+                  padding: EdgeInsets.only(bottom: 30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        child: Text(_currentArea,
+                            style: _currentAreaTextStyle), // 주차 구역
+                        padding: EdgeInsets.only(bottom: 10),
                       ),
+                      Text("위치 : " + _currentSpot,
+                          style: _infoTextStyle), // 주차장 위치
+                      Text("주차시간 : " + _parkingDuration,
+                          style: _infoTextStyle) // 주차 시간
+                    ],
+                  )),
+              Container(
+                padding: EdgeInsets.only(bottom: 30),
+                child: const Divider(height: 10, color: Colors.grey),
+              ),
+              Container(
+                  child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(bottom: 15),
+                    child: Row(
+                      children: [
+                        Icon(Icons.circle,
+                            size: 12,
+                            color: Colors.deepOrangeAccent), // 현재 위치 아이콘
+                        Container(
+                            child: Text('현재 위치', style: _infoTextStyle),
+                            padding: EdgeInsets.only(left: 5)),
+                        SizedBox(width: 10),
+                        Icon(Icons.rectangle,
+                            size: 12,
+                            color: Colors.deepOrangeAccent), // 주차 위치 아이콘
+                        Container(
+                            child: Text('주차 위치', style: _infoTextStyle),
+                            padding: EdgeInsets.only(left: 5)),
+                      ],
                     ),
-                    Text('지도를 클릭하면 확대해서 볼 수 있습니다',
-                        style: TextStyle(color: Colors.grey, fontSize: 12))
-                  ],
-                ))
-          ],
+                  ),
+                ],
+              )),
+              Flexible(
+                fit: FlexFit.tight,
+                child: WebView(
+                  initialUrl:
+                      "http://3.37.217.255:8080/swagger-ui/index.html#/",
+                  javascriptMode: JavascriptMode.unrestricted,
+                  onWebViewCreated: (WebViewController webViewController) {
+                    _controller = webViewController;
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );

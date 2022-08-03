217d769ec4c85c33a5cac01ee17dd95734d5f674
Minjune <minjune8506@gmail.com>
2022-08-04 01:11:36 +0900

    refactor : 프로젝트 구조 변경에 따른 import문 변경

--- lib/screens/bookmark_view.dart
+++ lib/screens/bookmark_view.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:parking_spot_frontend/menu.dart';
+import 'package:parking_spot_frontend/utility/menu.dart';
 
 class BookMarkView extends StatefulWidget {
   static const String routeName = "/BookMarkView";

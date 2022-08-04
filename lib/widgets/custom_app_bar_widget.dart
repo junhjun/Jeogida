43aee412f60dc7abce5fee78edd439b220e5a89b
Minjune <minjune8506@gmail.com>
2022-08-04 17:03:51 +0900

    fix : android, ios http 설정 파일

---
+++ lib/widgets/custom_app_bar_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+
+  const CustomAppBar({required this.title, Key? key}) : super(key: key);
+
+  final TextStyle titleStyle = const TextStyle(color: Colors.black);
+
+  @override
+  Size get preferredSize => Size.fromHeight(AppBar().preferredSize.height);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(title, style: titleStyle),
+      backgroundColor: Colors.white,
+    );
+  }
+}

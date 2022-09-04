1ddca0c55ff6cdf5b6fb6ff47387a3633cec628e
Minjune <minjune8506@gmail.com>
2022-09-04 22:35:37 +0900

    add : restart_app package

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -7,6 +7,7 @@
 // ignore_for_file: depend_on_referenced_packages
 
 import 'package:google_sign_in_web/google_sign_in_web.dart';
+import 'package:restart_app/restart_web.dart';
 import 'package:webview_flutter_web/webview_flutter_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
@@ -14,6 +15,7 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
   GoogleSignInPlugin.registerWith(registrar);
+  RestartWeb.registerWith(registrar);
   WebWebViewPlatform.registerWith(registrar);
   registrar.registerMessageHandler();
 }

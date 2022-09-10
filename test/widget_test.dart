5c13478d6e90379f2b1ad4bb9d8f2cd630273bca
Minjune <minjune8506@gmail.com>
2022-09-11 06:10:29 +0900

    fix : widget_test 오류 수정

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,12 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:parking_spot_frontend/home_view.dart';
+import 'package:parking_spot_frontend/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const HomeView());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);

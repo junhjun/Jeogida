fe06de9f312b5de38d3f428b7518c4d06445a69e
Minjune <minjune8506@gmail.com>
2022-09-11 06:09:35 +0900

    feat : bookmark service 개발

--- lib/services/bookmark_service.dart
+++ lib/services/bookmark_service.dart
@@ -1,28 +1,79 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:parking_spot_frontend/models/book_mark_car.dart';
 import '../models/book_mark_car_list.dart';
 import '../models/book_mark_space_list.dart';
 import '../utility/values.dart';
 
 class BookMarkService {
   static Future<BookMarkCarList> getBookmarkCarList(String userid) async {
-    final response = await http.get(Uri.parse(serverAddress + "car/" + userid));
+    final response = await http.get(Uri.parse("${serverAddress}car/$userid"));
     var responseBody = utf8.decode(response.bodyBytes);
     if (response.statusCode == 200) {
       return BookMarkCarList.fromJson(jsonDecode(responseBody));
     } else {
-      throw Exception("Failed to load bookMarkCar");
+      throw Exception("Fail to GET bookMarkCar");
     }
   }
 
-  static Future<BookMarkSpaceList> getBookmarkSpaceList() async {
+  static Future<BookMarkSpaceList> getBookmarkSpaceList(String userid) async {
     final response =
-        await http.get(Uri.parse(serverAddress + "favorite-location"));
+        await http.get(Uri.parse("${serverAddress}favorite-location/$userid"));
     var responseBody = utf8.decode(response.bodyBytes);
     if (response.statusCode == 200) {
       return BookMarkSpaceList.fromJson(jsonDecode(responseBody));
     } else {
-      throw Exception("Failed to load bookMarkSpace");
+      throw Exception("Fail to GET bookMarkSpace");
+    }
+  }
+
+  static void postBookMarkCar(int userCode, BookMarkCar car) async {
+    final response = await http.post(
+      Uri.parse("${serverAddress}car/$userCode"),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body:
+          jsonEncode(<String, String>{"name": car.name, "number": car.number}),
+    );
+    if (response.statusCode != 200 && response.statusCode != 201) {
+      throw Exception("Fail to POST bookMarkCar");
+    }
+  }
+
+  static void postBookMarkSpace(int userCode, int locationId) async {
+    final response = await http.post(
+      Uri.parse("${serverAddress}favorite-location/$locationId"),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+    if (response.statusCode != 200 && response.statusCode != 201) {
+      throw Exception("Fail to POST bookMarkSpace");
+    }
+  }
+
+  static void deleteBookMarkCar(int userCode, int carId) async {
+    final http.Response response = await http.delete(
+      Uri.parse("${serverAddress}car/$userCode/$carId"),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+    if (response.statusCode != 200) {
+      throw Exception("Fail to DELETE bookMarkCar");
+    }
+  }
+
+  static void deleteBookMarkSpace(int userCode, int locationId) async {
+    final http.Response response = await http.delete(
+      Uri.parse("${serverAddress}car/$userCode/$locationId"),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+    if (response.statusCode != 200) {
+      throw Exception("Fail to DELETE bookMarkSpace");
     }
   }
 }

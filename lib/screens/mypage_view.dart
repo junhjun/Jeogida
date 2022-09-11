83e95902f6aefefb38d33aecee5e7886c7ca61f2
HJ <chjoon23@nate.com>
2022-09-11 09:07:14 +0900

    마이페이지 UI 수정 : 가운데 배치

--- lib/screens/mypage_view.dart
+++ lib/screens/mypage_view.dart
@@ -14,7 +14,7 @@ class MyPageView extends StatefulWidget {
 
 class _MyPageViewState extends State<MyPageView> {
   final nameTextStyle =
-      const TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+      const TextStyle(fontSize: 27, fontWeight: FontWeight.bold);
   final iconColor = const Color(0xff75BCC6);
   final buttonStyle = TextButton.styleFrom(
     primary: Colors.black,
@@ -30,62 +30,63 @@ class _MyPageViewState extends State<MyPageView> {
     return Scaffold(
       backgroundColor: const Color(0xffededed),
       // padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 50),
-      body: Container(
-        height: MediaQuery.of(context).size.height * 0.35,
-        margin: const EdgeInsets.all(30),
-        padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-        decoration: BoxDecoration(
-            color: Colors.white, borderRadius: BorderRadius.circular(10)),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Center(
-              child: CircleAvatar(
-                radius: MediaQuery.of(context).size.width * 0.1,
-                backgroundImage:
-                    NetworkImage(context.watch<UserProvider>().user!.photoUrl!),
+      body: Center(
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.35,
+          margin: EdgeInsets.fromLTRB(30, 0, 30, 0),
+          padding: EdgeInsets.fromLTRB(0, 13, 0, 0),
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(15)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Center(
+                child: CircleAvatar(
+                  radius: MediaQuery.of(context).size.width * 0.1,
+                  backgroundImage:
+                      NetworkImage(context.watch<UserProvider>().user!.photoUrl!),
+                ),
               ),
-            ),
-            // const SizedBox(height: 20),
-            Center(
-              child: Container(
-                padding: const EdgeInsets.only(bottom: 15.0),
-                child: Text(context.watch<UserProvider>().user!.displayName!,
-                    style: nameTextStyle),
+              // const SizedBox(height: 20),
+              Center(
+                child: Container(
+                  padding: const EdgeInsets.only(bottom: 15.0),
+                  child: Text(context.watch<UserProvider>().user!.displayName!,
+                      style: nameTextStyle),
+                ),
               ),
-            ),
-            Container(
-              margin: const EdgeInsets.fromLTRB(35, 10, 20, 0),
-              // decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Icon(Icons.email, color: iconColor),
-                      Text(" ${context.watch<UserProvider>().user!.email!}",
-                          style: infoTextStyle)
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      TextButton.icon(
-                        onPressed: () {
-                          context.read<FindSpaceProvider>().clear();
-                          context.read<FindCarProvider>().clear();
-                          context.read<UserProvider>().handleLogOut();
-                          context.read<UserProvider>().clear();
-                          logger.i("Logout API Called");
-                        },
-                        icon: Icon(Icons.logout, color: iconColor),
-                        label: Text("로그아웃", style: infoTextStyle),
-                        style: buttonStyle,
-                      ),
-                    ],
-                  ),
-                ],
+              Container(
+                margin: EdgeInsets.fromLTRB(40, 10, 20, 0),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.email, color: iconColor),
+                        Text(" ${context.watch<UserProvider>().user!.email!}",
+                            style: infoTextStyle)
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        TextButton.icon(
+                          onPressed: () {
+                            context.read<FindSpaceProvider>().clear();
+                            context.read<FindCarProvider>().clear();
+                            context.read<UserProvider>().handleLogOut();
+                            context.read<UserProvider>().clear();
+                            logger.i("Logout API Called");
+                          },
+                          icon: Icon(Icons.logout, color: iconColor),
+                          label: Text("로그아웃", style: infoTextStyle),
+                          style: buttonStyle,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
